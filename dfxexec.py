3b8e077611b344309143bb17e39373ca317b918b
eyfel <110561078+eyfel@users.noreply.github.com>
2024-02-05 15:02:58 +0300

    v1.0

--- dfxexec.py
+++ dfxexec.py
@@ -1,51 +1,38 @@
 import ezdxf
 import pandas as pd
 
-# Function to extract attributes from text content
 def extract_attributes(text):
     attributes = {'Code': '', 'Material': '', 'Thickness': '', 'Quantity': ''}
     lines = text.split('\n')
     
     for line in lines:
-        if line.startswith('Code:'):
-            attributes['Code'] = line.replace('Code:', '').strip()
-        elif line.startswith('Material:'):
-            attributes['Material'] = line.replace('Material:', '').strip()
-        elif line.startswith('Thickness:'):
-            attributes['Thickness'] = line.replace('Thickness:', '').strip()
-        elif line.startswith('Quantity:'):
-            attributes['Quantity'] = line.replace('Quantity:', '').strip()
+        for key in attributes.keys():
+            if line.startswith(f'{key}:'):
+                attributes[key] = line.replace(f'{key}:', '').strip()
     
     return attributes
 
-# Function to extract code attributes from DXF file
-def extract_code_attributes_from_dxf(file_path):
+
+def extract_kod_texts_from_dxf(file_path):
     doc = ezdxf.readfile(file_path)
     msp = doc.modelspace()
 
-    code_attributes_list = []
+    kod_texts = []
 
     for text in msp.query('TEXT MTEXT'):
         text_content = text.plain_text().strip()
         if text_content.startswith("Code:"):
-            code_attributes = extract_attributes(text_content)
-            code_attributes_list.append(code_attributes)
+            kod_attributes = extract_attributes(text_content)
+            kod_texts.append(kod_attributes)
 
-    return code_attributes_list
+    return kod_texts
 
-# DXF file path
-dxf_file_path = "ornek.dxf"
+dxf_file_path = "example.dxf"
+extracted_kod_texts = extract_kod_texts_from_dxf(dxf_file_path)
 
-# Extract code attributes from the DXF file
-extracted_code_attributes = extract_code_attributes_from_dxf(dxf_file_path)
+df = pd.DataFrame(extracted_kod_texts)
 
-# Create a DataFrame from the extracted code attributes
-df = pd.DataFrame(extracted_code_attributes)
-
-# Excel file path
-excel_file_path = "parca_listesi.xlsx"
-
-# Save the DataFrame to an Excel file
+excel_file_path = "part_list.xlsx"
 df.to_excel(excel_file_path, index=False)
 
-print(f"Part List saved successfully to {excel_file_path}.")
+print(f"Part List successfully saved to {excel_file_path} file.")
